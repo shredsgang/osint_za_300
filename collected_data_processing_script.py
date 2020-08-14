1598edc1e33935d58a2ab44fbaae43c791b66488
ShredsGang <goshashreeeds@gmail.com>
2020-08-14 22:30:14 +0300

    Collected data processing

--- collected_data_processing_script.py
+++ collected_data_processing_script.py
@@ -17,17 +17,34 @@ for district_name in files:
 
         title = item["title"]
         address = item["address"]
+        vk = 'Не указано'
+        instagram = 'Не указано'
+
+        try:
+            social_links = item["socialLinks"]
+            for link in social_links:
+                if link['type'] == 'vkontakte':
+                    vk = link['href']
+                    print(vk)
+                elif link['type'] == 'instagram':
+                    instagram = link['href']
+                    print(instagram)
+
+        except KeyError:
+            vk = 'Не указано'
+            instagram = 'Не указано'
+
         try:
             metro = item["metro"][0]["name"]
         except IndexError:
             metro = 'Не указано'
+
         try:
             phone = item["phones"][0]["number"]
         except KeyError:
             phone = 'Нет телефона'
 
-        parsed_data = [title, district_name, address, metro, phone]
-        print(parsed_data)
+        parsed_data = [title, district_name, address, metro, phone, vk, instagram]
         df.loc[loc_index] = parsed_data
         loc_index += 1
 
